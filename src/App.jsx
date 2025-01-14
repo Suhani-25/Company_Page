794692953a0243b175daa3da6155455a7ae54318
Suhani <suhanibhardwaj252000@gmail.com>
2025-01-15 00:41:14 +0530

    m

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-//import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Herosection from "./Components/Herosection";
 import AboutPage from "./Pages/AboutPage";
